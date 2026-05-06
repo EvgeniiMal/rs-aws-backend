9fc6e410426370e5cf767c604e8a24fa1210d54e
EvgeniiMal <eugenemal88@gmail.com>
2026-05-06 21:30:18 +0200

    feat: use read db data in GET lambdas

--- product_service/src/handlers/product-list.ts
+++ product_service/src/handlers/product-list.ts
@@ -1,22 +1,72 @@
 import { APIGatewayProxyResult } from "aws-lambda";
 import { corsHeaders } from "../utils/cors-headers";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { BatchGetCommand, DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient();
+const dynamoDb = DynamoDBDocumentClient.from(client);
 
 export const getProductList = async (): Promise<APIGatewayProxyResult> => {
-  const products = await import("../data/product-list.json");
 
-  if (!products) {
+  try {
+    const productsResult = await dynamoDb.send(
+      new ScanCommand({
+        TableName: process.env.PRODUCTS_TABLE_NAME,
+      })
+    )
+    const products = productsResult.Items ?? [];
+
+    console.log(`Products query: ${products.length} product items received from DynamoDB`);
+
+    if (products.length === 0) {
+      return {
+        statusCode: 200,
+        headers: corsHeaders,
+        body: JSON.stringify(products),
+      };
+    }
+
+    const productIds = products.map((product) => product[process.env.PRODUCTS_TABLE_PRIMARY_KEY!]);
+    
+
+    const stocksResult = await dynamoDb.send(
+      new BatchGetCommand({
+        RequestItems: {
+          [process.env.STOCKS_TABLE_NAME!]: {
+            Keys: productIds.map((id) => ({
+              [process.env.STOCKS_TABLE_PRIMARY_KEY!]: id,
+            })),
+          },
+        },
+      })
+    )
+
+    const stocks = stocksResult?.Responses?.[process.env.STOCKS_TABLE_NAME!] ?? [];
+
+    console.log(`Stocks query: ${stocks.length} stock items received from DynamoDB`);
+
+    const fullProducts = products.map((product) => {
+      const stock = stocks.find((s) => s[process.env.STOCKS_TABLE_PRIMARY_KEY!] === product[process.env.PRODUCTS_TABLE_PRIMARY_KEY!]);
+      return {
+        ...product,
+        count: stock ? stock.count : 0,
+      }
+    });
+
     return {
-      statusCode: 500,
+      statusCode: 200,
       headers: corsHeaders,
-      body: JSON.stringify({
-        message: "Could not load products",
-      }),
+      body: JSON.stringify(fullProducts),
     };
+  } catch (error) {
+    console.error("Error fetching products:", error);
   }
 
   return {
-    statusCode: 200,
+    statusCode: 500,
     headers: corsHeaders,
-    body: JSON.stringify(products.default),
+    body: JSON.stringify({
+      message: "Could not load products",
+    }),
   };
 };
--- product_service/src/handlers/product.ts
+++ product_service/src/handlers/product.ts
@@ -1,27 +1,79 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import products from "../data/product-list.json";
 import { corsHeaders } from "../utils/cors-headers";
+import { isUUID } from "../utils/validate-uuid";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { GetCommand } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient();
+const dynamoDb = DynamoDBDocumentClient.from(client);
 
 export const getProduct = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   const id = event.pathParameters?.id;
 
-  const product = products.find((p) => String(p.id) === id);
-
-  if (!product) {
+  if (!id || !isUUID(id)) {
+    console.warn("Invalid product id:", id);
     return {
-      statusCode: 404,
+      statusCode: 400,
       headers: corsHeaders,
       body: JSON.stringify({
-        message: "Product not found",
+        message: "Invalid product id",
       }),
     };
   }
 
-  return {
-    statusCode: 200,
-    headers: corsHeaders,
-    body: JSON.stringify(product),
-  };
+  const getProductCommand = new GetCommand({
+    TableName: process.env.PRODUCTS_TABLE_NAME!,
+    Key: {
+      [process.env.PRODUCTS_TABLE_PRIMARY_KEY!]: id,
+    },
+  });
+
+  const getProductStockCommand = new GetCommand({
+    TableName: process.env.STOCKS_TABLE_NAME!,
+    Key: {
+      [process.env.STOCKS_TABLE_PRIMARY_KEY!]: id,
+    },
+  });
+
+  try {
+    const productResult = await dynamoDb.send(getProductCommand);
+    const product = productResult.Item;
+
+    if (!product) {
+      return {
+        statusCode: 404,
+        headers: corsHeaders,
+        body: JSON.stringify({
+          message: "Product not found",
+        }),
+      };
+    }
+
+    const stockResult = await dynamoDb.send(getProductStockCommand);
+    const stock = stockResult.Item;
+
+    const fullProduct = {
+      ...product,
+      count: stock ? stock.count : 0,
+    };
+
+    return {
+      statusCode: 200,
+      headers: corsHeaders,
+      body: JSON.stringify(fullProduct),
+    };
+  } catch (error) {
+    console.error("Error fetching product:", error);
+    return {
+      statusCode: 500,
+      headers: corsHeaders,
+      body: JSON.stringify({
+        message: "Internal server error",
+      }),
+    };
+  }
 };
+
