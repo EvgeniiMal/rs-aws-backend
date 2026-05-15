dcea2e8c127e9c40ced255264e76f4e89c53d20a
EvgeniiMal <eugenemal88@gmail.com>
2026-05-15 15:49:08 +0200

    feat: create deployment stack for products file import

--- import-service/deployment/lib/import-service-stack.ts
+++ import-service/deployment/lib/import-service-stack.ts
@@ -1,16 +1,70 @@
+import { aws_apigatewayv2 as apigV2 } from 'aws-cdk-lib';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cdk from 'aws-cdk-lib/core';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import path from 'path';
+import { HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+import { getContext } from '../utils/context';
+
+const PROJECT_ROOT = path.resolve(__dirname, '../../');
+const SRC_DIR = path.resolve(PROJECT_ROOT, 'src');
+const HANDLERS_DIR = path.resolve(SRC_DIR, 'handlers');
+const DEFAULT_RUNTIME = lambda.Runtime.NODEJS_24_X;
 
 export class ImportServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const { stage, productsBucketName, uploadPrefix } = getContext(this);
 
-    // The code that defines your stack goes here
+    const productsBucket = s3.Bucket.fromBucketName(
+      this, 'ProductsBucket', productsBucketName
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ImportServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const importProductsFileLambda = new NodejsFunction(
+      this, 'ImportProductsFileLambda', {
+      projectRoot: PROJECT_ROOT,
+      entry: path.join(HANDLERS_DIR, 'import-products-file.ts'),
+      handler: 'importProductsFile',
+      runtime: DEFAULT_RUNTIME,
+      environment: {
+        BUCKET_NAME: productsBucketName,
+        UPLOAD_PREFIX: uploadPrefix,
+      },
+    });
+
+    const importProductsFileIntegration = new HttpLambdaIntegration(
+      'ImportProductsFileIntegration',
+      importProductsFileLambda,
+    );
+
+    const api = new apigV2.HttpApi(this, 'ImportServiceApi', {
+      apiName: 'Import Service API',
+      description: 'API for importing product files',
+      corsPreflight: {
+        allowHeaders: ['Content-Type'],
+        allowMethods: [
+          apigV2.CorsHttpMethod.GET,
+          apigV2.CorsHttpMethod.OPTIONS,
+        ],
+        allowOrigins: ['*'],
+        maxAge: cdk.Duration.days(10),
+      },
+    });
+
+    api.addRoutes({
+      path: '/import',
+      methods: [apigV2.HttpMethod.GET],
+      integration: importProductsFileIntegration,
+    });
+
+    productsBucket.grantPut(importProductsFileLambda);
+
+    new cdk.CfnOutput(this, 'ImportServiceApiUrl', {
+      value: `${api.apiEndpoint}/import`,
+      description: 'Import Service API endpoint',
+      exportName: `${stage}-ImportServiceApiUrl`,
+    });
   }
 }
