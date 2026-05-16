c60ff81bceae4c60af14a0a7168a46f6f603c28c
EvgeniiMal <eugenemal88@gmail.com>
2026-05-16 11:59:16 +0200

    test: add test for file move

--- import-service/src/handlers/file-parser.spec.ts
+++ import-service/src/handlers/file-parser.spec.ts
@@ -3,7 +3,7 @@ import assert from "node:assert/strict";
 import { S3Event } from "aws-lambda";
 import { fileParser } from "./file-parser";
 import { Readable } from "node:stream";
-import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { CopyObjectCommand, DeleteObjectCommand, GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { mockClient } from "aws-sdk-client-mock";
 
 const s3Mock = mockClient(S3Client);
@@ -42,4 +42,22 @@ describe("fileParser", () => {
     assert.equal(s3Mock.commandCalls(GetObjectCommand).length, 1);
     assert.ok(true);
   });
+
+  it('should copy the file to the "parsed" directory and delete the original file',
+    async () => {
+      const csvStream = Readable.from([
+        'title,description,price,count\n',
+        'Test product,Some description,10,5\n',
+      ]);
+
+      s3Mock.on(GetObjectCommand).resolves({
+        Body: csvStream,
+      } as any);
+
+      await fileParser(mockEvent);
+
+      assert.equal(s3Mock.commandCalls(GetObjectCommand).length, 1);
+      assert.equal(s3Mock.commandCalls(CopyObjectCommand).length, 1);
+      assert.equal(s3Mock.commandCalls(DeleteObjectCommand).length, 1);
+    });
 });
