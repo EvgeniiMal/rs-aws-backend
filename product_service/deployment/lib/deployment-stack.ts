f9944949b7418790b6c1262258ffa64c55b9322b
EvgeniiMal <eugenemal88@gmail.com>
2026-05-23 18:52:58 +0200

    feat: create SNS topic and subscriptions

--- product_service/deployment/lib/deployment-stack.ts
+++ product_service/deployment/lib/deployment-stack.ts
@@ -4,6 +4,8 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as eventsources from 'aws-cdk-lib/aws-lambda-event-sources';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import { Construct } from 'constructs';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import path from 'path';
@@ -27,6 +29,8 @@ export class DeploymentStack extends cdk.Stack {
       productsTableName,
       stocksTablePrimaryKey,
       stocksTableName,
+      inStockProductsEmail,
+      outOfStockProductsEmail,
     } = context;
 
     const productTable = dynamodb.Table.fromTableName(
@@ -46,6 +50,29 @@ export class DeploymentStack extends cdk.Stack {
       batchSize: 5,
     });
 
+    const productsCreatedTopic = new sns.Topic(this, 'ProductsCreatedTopic', {
+      topicName: 'productsCreatedTopic',
+    });
+
+    productsCreatedTopic.addSubscription(
+      new subs.EmailSubscription(inStockProductsEmail!, {
+        filterPolicy: {
+          stockStatus: sns.SubscriptionFilter.stringFilter({
+            allowlist: ["in_stock"],
+          }),
+        }
+      })
+    );
+
+    productsCreatedTopic.addSubscription(
+      new subs.EmailSubscription(outOfStockProductsEmail!, {
+        filterPolicy: {
+          stockStatus: sns.SubscriptionFilter.stringFilter({
+            allowlist: ["out_of_stock"],
+          }),
+        }
+      })
+    );
 
     const catalogBatchProcess = new NodejsFunction(this, 'CatalogBatchProcess', {
       projectRoot: PROJECT_ROOT,
@@ -59,9 +86,9 @@ export class DeploymentStack extends cdk.Stack {
         PRODUCTS_TABLE_NAME: productTable.tableName,
         STOCKS_TABLE_PRIMARY_KEY: stocksTablePrimaryKey,
         STOCKS_TABLE_NAME: stocksTable.tableName,
+        PRODUCTS_CREATED_TOPIC_ARN: productsCreatedTopic.topicArn,
       },
     });
-
 
     catalogBatchProcess.addEventSource(catalogItemsEventSource);
 
@@ -146,6 +173,7 @@ export class DeploymentStack extends cdk.Stack {
     stocksTable.grantReadWriteData(catalogBatchProcess);
 
     catalogItemsQueue.grantConsumeMessages(catalogBatchProcess);
+    productsCreatedTopic.grantPublish(catalogBatchProcess);
 
     new cdk.CfnOutput(this, 'CatalogItemsQueueExport', {
       value: catalogItemsQueue.queueUrl,
