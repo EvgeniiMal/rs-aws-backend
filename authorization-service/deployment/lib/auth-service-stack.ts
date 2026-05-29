b9beaab4901806b6f7e166c5849a19d6bc9bc77a
EvgeniiMal <eugenemal88@gmail.com>
2026-05-29 18:02:40 +0200

    feat: create auth CDK stack

--- authorization-service/deployment/lib/auth-service-stack.ts
+++ authorization-service/deployment/lib/auth-service-stack.ts
@@ -4,9 +4,28 @@ import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
 
+const PROJECT_ROOT = path.resolve(__dirname, '../../');
+const SRC_DIR = path.resolve(PROJECT_ROOT, 'src');
+const HANDLERS_DIR = path.resolve(SRC_DIR, 'handlers');
+const DEFAULT_RUNTIME = Runtime.NODEJS_24_X;
 
 export class AuthorizationServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const basicAuthorizerLambda = new NodejsFunction(this, 'BasicAuthorizerHandler', {
+      projectRoot: PROJECT_ROOT,
+      entry: path.resolve(HANDLERS_DIR, 'basic-authorizer.ts'),
+      handler: 'basicAuthorizerHandler',
+      runtime: DEFAULT_RUNTIME,
+      environment: {
+        evgeniimal: 'TEST_PASSWORD',
+      }
+    });
+
+    new cdk.CfnOutput(this, 'BasicAuthorizerLambdaArn', {
+      value: basicAuthorizerLambda.functionArn,
+      exportName: 'BasicAuthorizerLambdaArn',
+    });
   }
 }
